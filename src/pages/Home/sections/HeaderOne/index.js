ffa420b1c46193e496aeca150800615a3356c9d5
lrudolph333 <trey.ru.iii@gmail.com>
2026-01-24 16:01:20 -0800

    rm firebase

--- src/pages/Home/sections/HeaderOne/index.js
+++ src/pages/Home/sections/HeaderOne/index.js
@@ -18,7 +18,6 @@ import { Box, Container, Grid, IconButton, Stack, useMediaQuery } from "@mui/mat
 
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
-import MKButton from "components/MKButton";
 import MKTypography from "components/MKTypography";
 
 // Images
@@ -84,90 +83,75 @@ function HeaderOne() {
                 >
                   {"challenging patriarchy by redefining thriving masculinity, together"}
                 </MKTypography>
-                {user.user == null ? (
-                  <Stack
-                    direction={isMdDown ? "column" : "row"}
-                    spacing={1}
-                    my={3}
-                    alignItems="center"
+                <Stack
+                  direction={isMdDown ? "column" : "row"}
+                  spacing={1}
+                  my={3}
+                  alignItems="center"
+                >
+                  <IconButton
+                    component="a"
+                    href="https://www.instagram.com/brothas_express/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    sx={{
+                      color: "white",
+                      backgroundColor: "#800020",
+                      width: "40px",
+                      height: "40px",
+                      mx: 1.5,
+                      transition: "all 0.3s ease",
+                      "&:hover": {
+                        backgroundColor: "#9a0026",
+                        transform: "scale(1.1)",
+                      },
+                    }}
                   >
-                    <IconButton
-                      component="a"
-                      href="https://www.instagram.com/brothas_express/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      sx={{
-                        color: "white",
-                        backgroundColor: "#800020",
-                        width: "40px",
-                        height: "40px",
-                        mx: 1.5,
-                        transition: "all 0.3s ease",
-                        "&:hover": {
-                          backgroundColor: "#9a0026",
-                          transform: "scale(1.1)",
-                        },
-                      }}
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
-                      <svg
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162 0 3.403 2.759 6.162 6.162 6.162 3.403 0 6.162-2.759 6.162-6.162 0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4 2.209 0 4 1.791 4 4 0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
-                          fill="currentColor"
-                        />
-                      </svg>
-                    </IconButton>
-                    <IconButton
-                      component="a"
-                      href="https://www.tiktok.com/@brothas.express?lang=en"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      sx={{
-                        color: "white",
-                        backgroundColor: "#800020",
-                        width: "40px",
-                        height: "40px",
-                        mx: 1.5,
-                        transition: "all 0.3s ease",
-                        "&:hover": {
-                          backgroundColor: "#9a0026",
-                          transform: "scale(1.1)",
-                        },
-                      }}
-                    >
-                      <svg
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"
-                          fill="currentColor"
-                        />
-                      </svg>
-                    </IconButton>
-                  </Stack>
-                ) : (
-                  <Stack
-                    direction={isMdDown ? "column" : "row"}
-                    spacing={1}
-                    my={3}
-                    alignItems="center"
+                      <path
+                        d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162 0 3.403 2.759 6.162 6.162 6.162 3.403 0 6.162-2.759 6.162-6.162 0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4 2.209 0 4 1.791 4 4 0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </IconButton>
+                  <IconButton
+                    component="a"
+                    href="https://www.tiktok.com/@brothas.express?lang=en"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    sx={{
+                      color: "white",
+                      backgroundColor: "#800020",
+                      width: "40px",
+                      height: "40px",
+                      mx: 1.5,
+                      transition: "all 0.3s ease",
+                      "&:hover": {
+                        backgroundColor: "#9a0026",
+                        transform: "scale(1.1)",
+                      },
+                    }}
                   >
-                    {/**TODO Make this button really pop, animate it */}
-
-                    <MKButton color="primary" target="_blank" href="https://discord.gg/R5Q3XEzSX9">
-                      Enter Express Nexus Discord
-                    </MKButton>
-                  </Stack>
-                )}
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </IconButton>
+                </Stack>
               </Grid>
               <Grid
                 item
